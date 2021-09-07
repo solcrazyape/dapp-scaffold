45f6bc5fb96de7c7bd75175b923c6b55962f39e1
Crazy Ape Coin <85198323+solcrazyape@users.noreply.github.com>
2021-09-07 08:35:18 -0700

    Update index.tsx

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -12,9 +12,8 @@ import { formatUSD } from "../../utils/utils";
 export const HomeView = () => {
   const { marketEmitter, midPriceInUSD } = useMarkets();
   const { tokenMap } = useConnectionConfig();
-  const SRM_ADDRESS = "SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt";
-  const SRM = useUserBalance(SRM_ADDRESS);
-  const SOL = useUserBalance(WRAPPED_SOL_MINT);
+  const CNFT_ADDRESS = "BNSE3Fth53PGYCoiNJqjwtGkNTn2UWMmF64L8gA39YB5";
+  const CNFT = useUserBalance(CNFT_ADDRESS);
   const { balanceInUSD: totalBalanceInUSD } = useUserTotalBalance();
 
   useEffect(() => {
@@ -34,23 +33,14 @@ export const HomeView = () => {
   return (
     <Row gutter={[16, 16]} align="middle">
       <Col span={24}>
-        <h2>Your balances ({formatUSD.format(totalBalanceInUSD)}):</h2>
-        <h2>
-          SOL: {SOL.balance} ({formatUSD.format(SOL.balanceInUSD)})
-        </h2>
         <h2 style={{ display: "inline-flex", alignItems: "center" }}>
-          <TokenIcon mintAddress={SRM_ADDRESS} /> SRM: {SRM?.balance} (
-          {formatUSD.format(SRM?.balanceInUSD)})
+          <TokenIcon mintAddress={CNFT_ADDRESS} /> CNFT: {CNFT?.balance} (
+          {formatUSD.format(CNFT?.balanceInUSD)})
         </h2>
       </Col>
 
       <Col span={12}>
         <WalletMultiButton type="ghost" />
-      </Col>
-      <Col span={12}>
-        <Link to="/faucet">
-          <Button>Faucet</Button>
-        </Link>
       </Col>
       <Col span={24}>
         <div className="builton" />
